b4ae47aacec0906ec6f0b8fa6bcaa078e2c96ba3
clauber <clauber.sportmg@gmail.com>
2021-02-08 04:12:00 -0300

    três funções jquery excluidas

--- jquery/playground/index.js
+++ jquery/playground/index.js
@@ -1,8 +1,3 @@
 $(function() {
-	// Vamos fazer todos os elementos button saltarem:
-	$("button").addClass("animated bounce");
-	// Você pode fazer todos os elementos da class tremerem:
-	$(".well").addClass("animated shake");
-	// Veja como você faria o elemento button como o id target3 desaparecer:
-	$("#target3").addClass("animated fadeOut");
+
 });

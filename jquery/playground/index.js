ed9f44fab55512d2d019eb5d6c9b620d7985ec78
clauber <clauber.sportmg@gmail.com>
2021-02-08 04:27:48 -0300

    adicionando classes ao mesmo elemento

--- jquery/playground/index.js
+++ jquery/playground/index.js
@@ -1,3 +1,7 @@
 $(function() {
-
+	// Os três seletores individuais vai acabar adicionando
+	// as três classes para o elemento button como o id #target1
+	$("button").addClass("animated");
+	$(".btn").addClass("shake");
+	$("#target1").addClass("btn-primary");
 });

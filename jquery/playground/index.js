0b173951928704e53f4d4c1d5e9000358adb19b7
clauber <clauber.sportmg@gmail.com>
2021-02-08 04:00:09 -0300

    elementos da class tremem

--- jquery/playground/index.js
+++ jquery/playground/index.js
@@ -1,4 +1,6 @@
 $(function() {
 	// Vamos fazer todos os elementos button saltarem:
-	$("button").addClass("animated bounce");	
+	$("button").addClass("animated bounce");
+	// Você pode fazer todos os elementos da class tremerem:
+	$(".well").addClass("animated shake");
 });

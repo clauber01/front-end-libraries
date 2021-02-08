26d1cce6a4b0d997e0f2e0c196958f11f2def200
clauber <clauber.sportmg@gmail.com>
2021-02-08 04:09:35 -0300

    elemento button com o id desaparece

--- jquery/playground/index.js
+++ jquery/playground/index.js
@@ -3,4 +3,6 @@ $(function() {
 	$("button").addClass("animated bounce");
 	// Você pode fazer todos os elementos da class tremerem:
 	$(".well").addClass("animated shake");
+	// Veja como você faria o elemento button como o id target3 desaparecer:
+	$("#target3").addClass("animated fadeOut");
 });
